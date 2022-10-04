12836715e063fe1bb7f659dc802f1226b5029793
Eduardo Finotti <eduardo.finotti@hotmail.com>
2022-10-04 18:02:47 -0300

    Criação da estrutura incial da pipeline

--- src/test/java/support/Commands.java
+++ src/test/java/support/Commands.java
@@ -22,7 +22,7 @@ public class Commands extends RunCucumber {
         System.out.println("##################################");
         try {
             System.out.println("Vai clicar no elemento: " + element);
-            waitElementBeClickable(element, 10000);
+            waitElementBeClickable(element, 10);
             getDriver().findElement(element).click();
             System.out.println("Clicou no elemento: " + element);
         } catch (Exception error){
@@ -36,7 +36,7 @@ public class Commands extends RunCucumber {
         System.out.println("##################################");
         try {
             System.out.println("Vai preencher o campo: " + element);
-            waitElementBeVisible(element, 10000);
+            waitElementBeVisible(element, 10);
             getDriver().findElement(element).sendKeys(value);
             System.out.println("Preencheu o campo: " + element);
         } catch (Exception error){
@@ -50,7 +50,7 @@ public class Commands extends RunCucumber {
         String actualMessage = "";
         System.out.println("##################################");
         System.out.println("Vai validar mensagem: " + expectedMessage);
-        waitElementBeVisible(element, 10000);
+        waitElementBeVisible(element, 10);
         actualMessage = getDriver().findElement(element).getText();
 
         // Quando não der certo, vai quebrar o teste!!!
---
+++ src/test/java/support/ScreenshotUtils.java
@@ -0,0 +1,23 @@
+package support;
+
+import cucumber.api.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static runner.RunBase.getDriver;
+
+public class ScreenshotUtils {
+
+    public static void addScreenshotOnScenario(Scenario scenario) {
+        System.out.println("===================");
+        System.out.println("Teste que executado: " + scenario.getName());
+        System.out.println("Status: " + scenario.getStatus());
+        System.out.println("Tag: " + scenario.getSourceTagNames());
+        System.out.println("===================");
+
+        if(scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+    }
+}
